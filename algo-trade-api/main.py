5b04feb28f969a9537139f62391757e66a751d91
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-06-26 20:47:12 -0400

    init get_pe_history method

--- algo-trade-api/main.py
+++ algo-trade-api/main.py
@@ -53,8 +53,36 @@ def get_keyStatistics(symbol:str=Path(min_length=1, max_length=5)):
          raise HTTPException(status_code=500, detail=f"Error fetching data: {str(e)}") 
 
 
+
+@app.get("/stock/{symbol}/fundamental/overview/pe-history")
+def get_pe_history(symbol:str=Path(min_length=1, max_length=5)):
+    try:
+        stock = yf.Ticker(symbol)
+        prices = stock.history(period="5y")
+        eps = stock.financials.loc["Diluted EPS"]
+
+        pe_history = []
+
+        for date, eps_value in eps.item():
+            year = date.year
+            year_prices = prices[prices.index]
+
+
+
+
+
+
+        return {
+            "prices" : prices["Close"].to_dict(), 
+            "eps" : eps.to_dict()
+        }
+
+    except HTTPException:
+        raise 
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching data: {str(e)}")
     
-
-
+        
 
 
