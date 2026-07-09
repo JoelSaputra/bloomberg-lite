b8688c58dd82ffa111799c8af4539ff689212935
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-07-09 02:36:38 -0400

    DetailedCompany fetched from tvkit

--- algo-trade-next/src/components/DetailedCompany.jsx
+++ algo-trade-next/src/components/DetailedCompany.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
+// TradingViewWidget.jsx
+import React, { useEffect, useRef, memo } from 'react';
 
+function TradingViewWidget() {
+  const container = useRef();
 
-const DetailedCompany = () => {
+  useEffect(
+    () => {
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+        {
+          "allow_symbol_change": true,
+          "calendar": false,
+          "details": true,
+          "hide_side_toolbar": false,
+          "hide_top_toolbar": false,
+          "hide_legend": false,
+          "hide_volume": false,
+          "hotlist": true,
+          "interval": "D",
+          "locale": "en",
+          "save_image": true,
+          "style": "1",
+          "symbol": "NASDAQ:AAPL",
+          "theme": "dark",
+          "timezone": "Etc/UTC",
+          "backgroundColor": "#0F0F0F",
+          "gridColor": "rgba(242, 242, 242, 0.06)",
+          "watchlist": [],
+          "withdateranges": true,
+          "range": "1D",
+          "compareSymbols": [],
+          "studies": [],
+          "autosize": true
+        }`;
+        const el = container.current;
+      container.current.appendChild(script);
+
+      return () => {
+      if(el){
+        el.innerHTML = ''
+      }
+    }
+    },
+    []
+  );
+
   return (
-    <div>
-
+    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
+      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
+      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener nofollow" target="_blank"><span className="blue-text">AAPL stock chart</span></a><span className="trademark"> by TradingView</span></div>
     </div>
-  )
+  );
 }
 
-export default DetailedCompany
+export default memo(TradingViewWidget);
--- algo-trade-next/src/pages/technicalLab.js
+++ algo-trade-next/src/pages/technicalLab.js
@@ -1,8 +1,20 @@
+import DetailedCompany from '@/components/DetailedCompany'
 import React from 'react'
 
 const technicalLab = () => {
   return (
-    <div>technicalLab</div>
+    <div className="flex flex-col space-y-10">
+      <div className="flex flex-row space-x-7">
+      
+        <div className='w-[100%] h-160 matte-metal-card border border-grey rounded-lg'>
+          <DetailedCompany/>
+        </div>
+
+      </div>
+
+      <div className='w-[40%] h-160 matte-metal-card border border-grey rounded-lg'></div>
+      
+    </div>
   )
 }
 
