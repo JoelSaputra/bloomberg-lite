0e981bae717f7fddeb0e614743cf77e5449d1703
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-07-14 02:56:42 -0400

    save

--- algo-trade-api/live_price.py
+++ algo-trade-api/live_price.py
@@ -1,15 +1,14 @@
 import yfinance as yf
 
 SYMBOLS = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA", "META"]
-price_cache = {}
+symbols_info = {}
 
 
 def message_handler(message):
     print("Received message:", message)
+    symbols_info[message['id']] = message
 
-
-with yf.WebSocket() as ws:
-    ws.subscribe(SYMBOLS)
-    ws.listen(message_handler)
-
-
+def price_stream():
+    with yf.WebSocket() as ws:
+        ws.subscribe(SYMBOLS)
+        ws.listen(message_handler)
